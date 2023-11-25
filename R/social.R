7ed9e04d5068bd22f6e367a2ce6b27588a297e1a
Gchism94 <gchism@arizona.edu>
2023-11-24 19:01:05 -0700

    Only use span elements

--- R/social.R
+++ R/social.R
@@ -10,10 +10,9 @@
 #' @param github_icon HTML character entity for GitHub icon (default: "&#xf09b").
 #' @param linkedIn_icon HTML character entity for LinkedIn icon (default: "&#xf08c").
 #' @param web_icon HTML character entity for a generic web icon (default: "&#xf0ac").
-#' @param bg_color Background color for the HTML div (default: "#fefefe").
-#' @param icon_color Color for the icons (default: "#006199").
-#' @param font_color Color for the text (default: "#006199").
-#' @param font_family Font family for the text (default: "roboto").
+#' @param icon_col Color for the icons (default: "#006199").
+#' @param text_col Color for the text (default: "#006199").
+#' @param body_font Font family for the text (default: "roboto").
 #' @return An HTML string representing the formatted social media details.
 #' @importFrom glue glue
 #' @examples
